b29da1b01983e5ef3efda0ee33127148215130a0
Ren Shan <ren.shan@cabinjohnconsulting.com>
2025-03-02 21:22:54 -0600

    organize files

--- linkgear_mining_dao_token/run.sh
+++
@@ -1,9 +0,0 @@
-leo run transfer_private "{owner: aleo1g24j0aa2fw69y7n05aehk3gp7cf8tr9ea2nnpnavx3qdpmc5r5rse08kl2.private, 
-amount: 1000000000000000u128.private, 
-token_id: 71619063553950105623552field.private, 
-external_authorization_required: true.private, 
-authorized_until: 4294967295u32.private, 
-_nonce: 4171342428487949176087471229466059149427987590264190336989689461864714570288group.public }" "{ owner: aleo1g24j0aa2fw69y7n05aehk3gp7cf8tr9ea2nnpnavx3qdpmc5r5rse08kl2.private, 
-amount_spent: 0u128.private, 
-epoch_spent: 0u32.private, 
-_nonce: 752514431784115870595676085969032676294182558689481203097690104756728941221group.public }" "100_000_000u128" "aleo1q5taq9mdcq7cvv4mq67fmvum2my3z2qzszgwm9ju70eplprx0sfqnszn4m" "1u32"
